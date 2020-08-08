90c5df2757004a0ae1f1d14300d20ec3b8f24a8d
Daniel Navarrete <cdng9202@gmail.com>
2020-08-08 11:41:59 -0500

    Add contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,18 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
   plugins: [
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-emotion`
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
+      },
+    },
   ],
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const StyledHeading = styled.h1`
   font-size: 32px;
 `;
 
-const Heading = () => <StyledHeading className="bg-red-500">Daniel Navarrete</StyledHeading>;
+const Heading = () => {
+  const {
+    contentfulLandingPage: {
+      heading,
+      description: { description },
+    },
+  } = useStaticQuery(graphql`
+    query MyQuery {
+      contentfulLandingPage {
+        heading
+        description {
+          description
+        }
+      }
+    }
+  `);
+  return (
+    <div>
+      <StyledHeading className="bg-red-500">{heading}</StyledHeading>
+      <p>{description}</p>
+    </div>
+  );
+};
 
 export default Heading;
-// 
